028cd30a71515c80ebc8126362c9edd110212356
ashtika thapa <ashtikathapa@gmail.com>
2026-07-14 23:44:46 +0530

    Added Kadane's Algorithm solution for Maximum Subarray

---
+++ src/Max_subarray.java
@@ -0,0 +1,22 @@
+public class Max_subarray {
+    public static int maxSubarray(int[] nums) {
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            sum+=nums[i];
+            if (sum > max) {
+                max = sum;
+            }
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int answer = maxSubarray(nums);
+        System.out.println("maximum subarray sum =" + answer);
+    }
+}
